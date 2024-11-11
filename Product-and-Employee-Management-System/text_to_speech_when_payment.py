1422513b85cbd60ac0b28757cde6620a9eafb188
nguyenhuuton123 <nhtonx1@gmail.com>
2024-11-11 21:59:53 +0700

    update structure

--- Product-and-Employee-Management-System/text_to_speech_when_payment.py
+++
@@ -1,8 +0,0 @@
-from gtts import gTTS
-import os
-from playsound import playsound
-import sys
-tts = gTTS("Số tiền đưa lại cho khách hàng là " + sys.argv[1] ,tld='com.vn',lang='vi')
-
-tts.save("notification_by_speech1.mp3")
-playsound("notification_by_speech1.mp3")
